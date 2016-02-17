2e535071355e3c84b72ab5ecce71920a52717526
tsukijihajime <tsukiji@mmi-sc.co.jp>
2016-02-17 13:34:27 +0900

    広告アプリ新規作成

---
+++ app/src/test/java/mmisc/co/jp/bannerexample/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package mmisc.co.jp.bannerexample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
